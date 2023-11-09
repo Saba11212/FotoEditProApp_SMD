cbf2826bdf6d1ba1b7942f532905663cc4021ffa
Saba Karim <i200813@nu.edu.pk>
2023-11-09 21:46:30 +0500

    edit screen

---
+++ app/src/main/java/com/sabakarim/fotoeditpro_smd/edit.java
@@ -0,0 +1,14 @@
+package com.sabakarim.fotoeditpro_smd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
