533a8828c61848c19c367c0de9bc1ef2422af10b
Saba Karim <i200813@nu.edu.pk>
2023-11-09 22:30:19 +0500

    filter screen

--- app/src/main/java/com/sabakarim/fotoeditpro_smd/filter.java
+++ app/src/main/java/com/sabakarim/fotoeditpro_smd/filter.java
@@ -3,25 +3,17 @@ package com.sabakarim.fotoeditpro_smd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class edit extends AppCompatActivity {
+public class filter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit);
-        ImageView filter = findViewById(R.id.filter);
-        filter.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                // Handle the click event, navigate to forgot_pass.xml
-                Intent intent = new Intent(getApplicationContext(), filter.class); // Replace with the actual name of your activity
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_filter);
         ImageView back = findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
