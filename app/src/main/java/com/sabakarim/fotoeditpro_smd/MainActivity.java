8974b00c5ce0958311add1ef45e924ccc784e8dc
Saba Karim <i200813@nu.edu.pk>
2023-11-09 19:41:49 +0500

    splash screen updated

--- app/src/main/java/com/sabakarim/fotoeditpro_smd/MainActivity.java
+++ app/src/main/java/com/sabakarim/fotoeditpro_smd/MainActivity.java
@@ -2,13 +2,24 @@ package com.sabakarim.fotoeditpro_smd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final long SPLASH_DELAY = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Create a Handler to delay the navigation to the next screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_DELAY);
     }
 }
